1bd84efefb4d30e8c874d47553b70316af3d998f
Ricardo Salcido <rsalcido@liberty.edu>
2019-01-18 15:47:17 -0600

    "grade classwork"

---
+++ GradeWork111819.py
@@ -0,0 +1,46 @@
+# Exercise 1:
+#
+# Print -20 to and including 50. Use any loop you want.
+#
+def main():
+    number3()
+    # number2()
+# #     number1()
+# #
+# # def number1():
+# #     nums = [-20,50,1]
+# #
+# #     for eachEL in nums:
+# #         print(eachEL)
+# #
+# #
+# #
+# #
+# #
+# #
+# # if __name__ == '__main__':
+# #     main()
+# #
+
+#
+#     Exercise 2:
+#
+# Create a loop that prints even numbers from 0 to and including 20.
+# def number2():
+#     for num in range(21):
+#         if num %2 ==0:
+#             print(num)
+# Exercise 3:
+#
+# Prompt the user for 3 numbers. Then print the 3 numbers along with their average after the 3rd number is entered.
+# Refer to example below replacing NUMBER1, NUMBER2, NUMBER3, and THEAVERAGE with the actual values.
+
+
+def problem3():
+    a= int(input("Please enter a number")
+
+
+
+
+if __name__ == '__main__':
+    main()
